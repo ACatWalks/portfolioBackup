cca0f4fd89c6e7018d066463dd1a8a331717b637
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-10-19 16:23:25 -0700

    First commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import Nav from '../views/Nav'
+import Footer from '../views/Footer'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import About from '../views/About'
+import Contact from '../views/Contact'
+import Projects from '../views/Projects'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
