9401a5212e078fd155317f525ed2d70bba95497c
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-12-31 12:54:26 -0800

    About page is now complete

--- portfolio/src/views/About.js
+++ portfolio/src/views/About.js
@@ -6,6 +6,13 @@ function About() {
     return (
         <div className='about'>
             <Nav />
+            <div className='hero'>
+                <div className='hero-txt'>
+                <p>I am</p>
+                <h1>AMANDA WOODS</h1>
+                <h3>Full-Stack Developer in San Diego</h3>
+                </div>
+            </div>
             <h2 id='headline'>About Me</h2>
             <div className='container'>
                     <div className='left'>
@@ -13,9 +20,7 @@ function About() {
                         <h4>Problem Solver. Developer. Visual Storyteller.</h4>
                         <p>I currently work as a substitute teacher, but I've recently completed a software development course in which I have gained fundamental knowledge of Javascript, React, HTML, CSS, and SCSS among others, to the extent that I made a responsive browser game modeled on the board game Clue. I also created an app that allows substitute teachers to rate school districts and schools and a telehealth app that allows patients to chat with a doctor. In my current position I succeed by identifying issues before they happen and being flexible about taking on new challenges. I found that I enjoy applying these skills to software development as well and I am passionate about designing technological solutions to real-world problems, so I'm targeting a software development role, preferably front end.</p>
                     </div>
-                    <div className='right'>
-                    <p>Insert Photo Here</p>
-                    </div>
+                    <div className='right'></div>
             </div>
             <h2 id='skills'>Skills</h2>
             <div className='card-container'>
