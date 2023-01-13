1f44f14ff637648cbabd1dd37f9d71ed5dcd69a7
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2023-01-13 13:11:48 -0800

    Now with media queries

--- portfolio/src/views/Contact.js
+++ portfolio/src/views/Contact.js
@@ -13,9 +13,9 @@ function Contact() {
             <h1>Contact Me</h1>
             <ul>
                 <li className="email"><span className='fa-icon'><FaEnvelope /></span><strong>Email</strong>:  <a href="mailto:acwoods314@gmail.com">acwoods314@gmail.com</a></li>
-                <li className="github" style={{position: 'relative', left: '20px'}}><span className='fa-icon'><FaGithub /></span><strong>GitHub</strong>:     <a href="https://github.com/ACatWalks">https://github.com/ACatWalks</a></li>
-                <li className="linkedin" style={{position: 'relative', left: '78px'}}><span className='fa-icon'><FaLinkedin /></span><strong>LinkedIn</strong>:   <a href="https://www.linkedin.com/in/amandacwoods/">https://www.linkedin.com/in/amandacwoods/</a></li>
-                <li className="resume" style={{position: 'relative', right: '24px'}}><span className='fa-icon'><FaFileDownload /></span><strong>Resume</strong>:     <button><a href="https://drive.google.com/file/d/1PE26OhotmfFHxcQUovIsUfP34lnSV3uI/view?usp=sharing" style={{color: '#F5FFFA'}} download>Download/Print</a></button></li>
+                <li className="github"><span className='fa-icon'><FaGithub /></span><strong>GitHub</strong>:     <a href="https://github.com/ACatWalks">https://github.com/ACatWalks</a></li>
+                <li className="linkedin"><span className='fa-icon'><FaLinkedin /></span><strong>LinkedIn</strong>:   <a href="https://www.linkedin.com/in/amandacwoods/">https://www.linkedin.com/in/amandacwoods/</a></li>
+                <li className="resume"><div className='resume-container'><span className='fa-icon'><FaFileDownload /></span><strong>Resume</strong>:     <button><a href="https://drive.google.com/file/d/1PE26OhotmfFHxcQUovIsUfP34lnSV3uI/view?usp=sharing" style={{color: '#F5FFFA'}} download>Download/Print</a></button></div></li>
             </ul>
             <Footer />
         </div>
